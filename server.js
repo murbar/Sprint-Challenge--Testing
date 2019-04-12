faf9683936ccd28dab50e1f46496022b396e93bd
Joel Bartlett <joel@murbar.com>
2019-04-12 12:11:42 -0400

    Add POST route

--- server.js
+++ server.js
@@ -5,8 +5,16 @@ const db = require('./db');
 
 server.use(express.json());
 
+const gameIsValid = game => game.title && game.genre && game.releaseYear;
+
 server.post('/games', async (req, res) => {
-  res.status(200).json();
+  const { body: game } = req;
+  if (!gameIsValid(game)) {
+    res.status(422).json({ error: 'A title, genre, and releaseYear are required.' });
+  } else {
+    const newGame = db.create(game);
+    res.status(201).json(newGame);
+  }
 });
 
 module.exports = server;
